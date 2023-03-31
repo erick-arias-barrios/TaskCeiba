e628739bda7455145ce357c6e7bdb4c69ba0fd9c
Erick Arias <erick.arias.barrios@gmail.com>
2023-03-31 15:54:37 -0500

    Initial Commit

---
+++ TaskCeiba/TaskCeibaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskCeibaApp.swift
+//  TaskCeiba
+//
+//  Created by Erick Arias on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskCeibaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
